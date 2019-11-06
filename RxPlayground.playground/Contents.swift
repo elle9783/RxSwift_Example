766d4492d5deddf253eed4288a31b0fe9f8d8f3e
Nakama <nakama@203309.local>
2019-11-06 21:11:09 +0700

    Introduction-to-RxSwift

--- RxPlayground.playground/Contents.swift
+++ RxPlayground.playground/Contents.swift
@@ -2,4 +2,36 @@ import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
 import RxSwift
 
-let number = Observable<String>.of("satu")
+let disposeBag = DisposeBag()
+//let number = Observable<String>.of("satu", "dua", "tiga")
+//
+//number.subscribe(onNext: { (value) in
+//    print(value)
+//}, onError: { (error) in
+//    print(error)
+//}, onCompleted: {
+//    print("complete")
+//}, onDisposed:  {
+//    print("dispose")
+//}).disposed(by: disposeBag)
+
+
+
+
+
+//number sebagai observable, dan x sebagai observer
+//ketika disposed, x yang akan di release
+let number = PublishSubject<String>()
+let x = number.subscribe(onNext: { (value) in
+    print("haha \(value)")
+}, onDisposed: {
+    print("disposed")
+}
+                 ).disposed(by: disposeBag)
+
+number.onNext("satu")
+print("do something")
+number.onNext("dua")
+number.onCompleted()
+number.onNext("lima")
+
